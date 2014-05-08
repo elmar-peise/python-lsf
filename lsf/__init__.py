ccbf3f67adbdcd676c2c6d989707c07b0f73fdbc
Elmar Peise <peise@aices.rwth-aachen.de>
2014-05-08 02:06:56 +0200

    removed old files form module

--- lsf/__init__.py
+++ lsf/__init__.py
@@ -1,16 +1,4 @@
-__all__ = ["LSFError", "Job", "submit", "Joblist", "Host", "Hostlist", "ejobs",
-           "ehosts", "esub"]
+__all__ = ["ejobs" "ehosts"]
 
-from error import LSFError
-import job
-import joblist
-import host
-import hostlist
-Job = job.Job
-Joblist = joblist.Joblist
-Host = host.Host
-Hostlist = hostlist.Hostlist
-from submit import submit
 import ejobs
 import ehosts
-import esub
