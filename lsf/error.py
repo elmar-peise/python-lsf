17715bf3361f4193aa9f17d81aeeca220b11a454
Elmar Peise <peise@aices.rwth-aachen.de>
2014-05-09 22:18:48 +0200

    removed old code

--- lsf/error.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-from __future__ import print_function, division
-
-
-class LSFError(EnvironmentError):
-    """Errors from LSF"""
-
-    def __init__(self, *t):
-        EnvironmentError.__init__(self, *t)
